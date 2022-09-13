e3efe5d9f91105a9971e1db911fcc1dad8fee4a6
신창호 <vkdghckdh@gmail.com>
2022-09-13 16:24:16 +0900

    :sparkles: feat: server 초기 환경설정

---
+++ server/nosleep/src/main/java/mainproject/nosleep/NosleepApplication.java
@@ -0,0 +1,13 @@
+package mainproject.nosleep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NosleepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NosleepApplication.class, args);
+	}
+
+}
