858d2818c6426e7ca5f495c844efdd9f93e72a00
hyunjun Yang <yhj0439@nate.com>
2022-09-13 17:04:24 +0900

    :sparkles: input 컴포넌트 & Radio 컴포넌트 완료

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Input from './component/Input';
+import RadioButton from './component/RadioButton';
 
 function App() {
   return (
     <div className="App">
-
+      {/* <Input color={'gray'}></Input> */}
+      <Input color={'yellow'}></Input>
+      <RadioButton />
     </div>
   );
 }
