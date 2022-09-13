cf5b8f26df717f776c81c0f58d693a04fc6ce053
Seo jongryeol <hushcyon@gmail.com>
2022-09-13 15:25:54 +0900

    button

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Button from './component/Button';
 
 function App() {
   return (
     <div className="App">
-
+        <Button buttonStyle={'sub'} > 예시입니다 </Button>
+        <Button buttonStyle={'main'} > 예시입니다 </Button>
+        <Button buttonStyle={'etc'} width="12opx"> 예시입니다 </Button>
     </div>
   );
 }
---
+++ front/src/component/Button.js
@@ -0,0 +1,51 @@
+import React from "react";
+import styled from "styled-components";
+
+const ButtonStyled = styled.div`
+    button {
+    height: 44px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 14px;
+    font-weight: bold;
+    border-radius: 3px;
+    padding: 14px;
+    }
+`
+
+const MainButton = styled.button`
+    width: ${(props) => props.width || "342px"};
+    background-color: var(--mainYellow);
+    color: var(--mainDeepGray) ;
+    border: 2px solid var(--mainYellow);
+`
+
+const SubMainButton = styled.button`
+    width: ${(props) => props.width || "342px"};
+    background-color : var(--mainDeepGray);
+    color : var(--mainYellow);
+    border : 2px solid var(--mainYellow);
+    
+`
+
+const EtcButton = styled.button`
+    width: ${(props) => props.width || "342px"};
+    background-color : var(--mainLighitGray);
+    color : white;
+    border : 2px solid var(--mainLighitGray);
+`
+
+
+function Button ({ children, ...props }) {
+    const style = props.buttonStyle
+    console.log(style)
+
+    return(
+        <ButtonStyled>
+            { style === 'main' ?  <MainButton {...props}> {children} </MainButton> : style === 'sub'? <SubMainButton {...props}> {children} </SubMainButton> :  <EtcButton {...props}> {children} </EtcButton> }
+        </ButtonStyled>
+    )
+  }
+  
+export default Button;
