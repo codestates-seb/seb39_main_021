49a2a5834a0cdf7bb3b8df3eb9dbfe71e26b3b48
Hyunjun <96432772+JadeHyun@users.noreply.github.com>
2022-09-19 11:21:49 +0900

    Main page (#7)
    * home.js 화면 구성 및 배열의 map 메서드 사용하여 화면에 뿌려주기 - 1차 멘토링
    
    * main 페이지 카테고리 및 인기 후기 레이아웃 완료
    
    * menu 버튼클릭시 메 페이지 이동으로 해결
    
    * items.json 파일 수정

---
+++ front/src/mainPage/Router.js
@@ -0,0 +1,44 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import Header from "./header";
+import Home from "./Home";
+import Menu from "./menu";
+// import Store from "./Store";
+
+const Router = () => {
+  const [openMenu, setOpenMenu] = useState(true);
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+  return (
+    <BrowserRouter>
+      {openMenu ? (
+        <>
+          <Header toggleMenu={toggleMenu} openMenu={openMenu} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/food" element={<Store />}>
+          식당
+        </Route>
+        <Route path="/cafe">카페</Route>
+        <Route path="/animal">동물병원</Route>
+        <Route path="/pharmacy">약국</Route>
+        <Route path="/hospital">병원</Route>
+        <Route path="/sing">노래방</Route>
+        <Route path="/laundry">세탁방</Route>
+        <Route path="/convenience">편의점</Route>
+        <Route path="/pc">pc방</Route>
+        <Route path="/gas">주유소</Route>
+        <Route path="/unmanned">무인판매점</Route>
+        <Route path="/etc">기타 등등</Route> */}
+            <Route path="/login" element={<Menu />} />
+          </Routes>
+        </>
+      ) : (
+        <Menu toggleMenu={toggleMenu} openMenu={openMenu} />
+      )}
+    </BrowserRouter>
+  );
+};
+
+export default Router;
