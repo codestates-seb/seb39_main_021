6c1b3208a041020a630ce417ec6fb6be56680382
hyunjun Yang <yhj0439@nate.com>
2022-09-19 14:47:02 +0900

    merge 전 코드 저장

--- front/src/App.js
+++ front/src/App.js
@@ -2,9 +2,6 @@ import "./App.css";
 import Router from "./mainPage/Router";
 
 function App() {
-  // fetch("https://jsonplaceholder.typicode.com/posts")
-  //   .then((response) => response.json())
-  //   .then((json) => console.log(json));
   return (
     <div className="App">
       <Router />
