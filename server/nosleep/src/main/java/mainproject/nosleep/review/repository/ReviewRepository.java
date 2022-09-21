bc2aceb3bd3a40f912991673304df7a0bab49040
신창호 <vkdghckdh@gmail.com>
2022-09-21 15:11:30 +0900

    :sparkles: feat: 이용후기(Review) 추가에 따른 평균 평점 구하는 `Query`문 추가

--- server/nosleep/src/main/java/mainproject/nosleep/review/repository/ReviewRepository.java
+++ server/nosleep/src/main/java/mainproject/nosleep/review/repository/ReviewRepository.java
@@ -1,10 +1,24 @@
 package mainproject.nosleep.review.repository;
 
 import mainproject.nosleep.review.entity.Review;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+//테트트용
+    @Query(value="select AVG(r.rating), COUNT(r.rating) From Review r ", nativeQuery = true) // 쿼리문 테스트
+    List<Object[]> sumAndCountTestAllRating();
+
+    @Query(value="select AVG(r.rating) From Review r WHERE r.shop_id = :shopId  GROUP BY shop_id", nativeQuery = true)
+    List<Object[]> averageAndCountAllRating(@Param("shopId")Long shopId);
 
 }
