6ef65c41da97dc063d75f72b30e474af2947041f
신창호 <vkdghckdh@gmail.com>
2022-09-21 15:20:02 +0900

    :sparkles: feat: shop 갱신 기능 추가

--- server/nosleep/src/main/java/mainproject/nosleep/shop/repository/ShopRepository.java
+++ server/nosleep/src/main/java/mainproject/nosleep/shop/repository/ShopRepository.java
@@ -2,8 +2,17 @@ package mainproject.nosleep.shop.repository;
 
 import mainproject.nosleep.shop.entity.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ShopRepository extends JpaRepository<Shop, Long> {
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE Shop s set s.ratingAVG = :ratingAVG , s.review_count = s.review_count + 1 WHERE s.id = :shopId", nativeQuery = true)
+    void updateRatingAVG(@Param("shopId") Long shopId,@Param("ratingAVG") Double ratingAVG);
 }
