d4e548c006ad3796a952924e05c3b07cf035991b
신창호 <vkdghckdh@gmail.com>
2022-09-21 15:17:54 +0900

    :sparkles: feat: shop 응답 DTO 구현

---
+++ server/nosleep/src/main/java/mainproject/nosleep/shop/dto/ShopResponseDto.java
@@ -0,0 +1,55 @@
+package mainproject.nosleep.shop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import mainproject.nosleep.review.dto.ReviewResponseDto;
+import mainproject.nosleep.review.entity.Review;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ShopResponseDto {
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class TotalPage{
+        //이미지, 가게명, 주소, 평점
+        private String image;
+        private String name;
+        private String address;
+        private Double ratingAVG;
+    }
+
+    @Setter
+    @Getter
+    @NoArgsConstructor
+    public static class DetailPage{
+        //사업장 명
+        private String name;
+        //주소
+        private String address;
+        //설명
+        private String detail;
+
+        //좌표
+        private Double longitude;
+        private Double latitude;
+
+        private Double ratingAVG;
+        //이용후기 리스트
+        private List<ReviewResponseDto.ShortReview> reviews; // reviewDTO 필요
+
+        public void setReviews(List<Review> reviews) {
+            this.reviews = reviews.stream().map(
+                    review -> {
+                        ReviewResponseDto.ShortReview shortReview = new ReviewResponseDto.ShortReview();
+                        shortReview.setId(review.getId());
+                        shortReview.setWriter(review.getWriter());
+                        shortReview.setContent(review.getContent());
+                        return shortReview;
+        }).collect(Collectors.toList());
+        }
+    }
+}
--- server/nosleep/src/main/java/mainproject/nosleep/shop/mapper/ShopMapper.java
+++ server/nosleep/src/main/java/mainproject/nosleep/shop/mapper/ShopMapper.java
@@ -1,11 +1,16 @@
 package mainproject.nosleep.shop.mapper;
 
 import mainproject.nosleep.shop.dto.ShopRequestDto;
+import mainproject.nosleep.shop.dto.ShopResponseDto;
 import mainproject.nosleep.shop.entity.Shop;
 import org.mapstruct.Mapper;
+
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface ShopMapper {
      Shop shopPostToShop(ShopRequestDto.Post postRequestBody);
      Shop shopPatchToShop(ShopRequestDto.Patch postRequestBody);
+     ShopResponseDto.DetailPage shopToDetailPage(Shop shop);
+
 }
