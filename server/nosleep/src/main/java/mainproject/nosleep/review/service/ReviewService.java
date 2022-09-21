d2552648e48575380343863b6b9a693f32fa1933
신창호 <vkdghckdh@gmail.com>
2022-09-21 15:14:57 +0900

    :sparkles: feat: ReviewService 평균평점 및 이용후기 Update기능추가

--- server/nosleep/src/main/java/mainproject/nosleep/review/service/ReviewService.java
+++ server/nosleep/src/main/java/mainproject/nosleep/review/service/ReviewService.java
@@ -4,19 +4,27 @@ import lombok.RequiredArgsConstructor;
 import mainproject.nosleep.review.entity.Review;
 import mainproject.nosleep.review.repository.ReviewRepository;
 import mainproject.nosleep.shop.entity.Shop;
+import mainproject.nosleep.shop.repository.ShopRepository;
+import mainproject.nosleep.shop.service.ShopService;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
     private final ReviewRepository reviewRepository;
-
+    private final ShopRepository shopRepository;
     public Review createReview(Review review){
 
-        return reviewRepository.save(review);
-
+        Review save = reviewRepository.save(review);
+        //평점 갱신
+        Double ratingAverage = findRatingAverage(save.getShop().getId());
+        // 이용후기 + 1, 평점 갱신
+        shopRepository.updateRatingAVG(save.getShop().getId(), ratingAverage);
+        return save;
     }
     public Review updateReview(Long id, Review review){
         Review updateReview = findVerifiedReview(id);
@@ -42,5 +50,10 @@ public class ReviewService {
                 ()-> new IllegalArgumentException("존재하지않는 이용후기 입니다.")
         );
     }
+    public Double findRatingAverage(Long shopId){
+        List<Object[]> objects = reviewRepository.averageAndCountAllRating(shopId);
+        return (Double) objects.get(0)[0];
+    }
+
 
 }
