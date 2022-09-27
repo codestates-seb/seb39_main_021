6eadada450061c47089aae69d6d98477370a3ecc
Seo jongryeol <hushcyon@gmail.com>
2022-09-27 19:21:06 +0900

    merge 전 커밋

--- front/src/storeDetailPage/Review.js
+++ front/src/storeDetailPage/Review.js
@@ -1,8 +1,21 @@
 import styled from "styled-components";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 import Button from "../component/Button";
+import { ReactComponent as Star } from "../asset/star.svg";
 
 const Review = () => {
+  const [onClick, setOnClick] = useState(false);
+  const [star, setStar] = useState("#ffffff");
+
+  const changeStar = () => {
+    onClick ? setOnClick(false) : setOnClick(true);
+    onClick ? setStar("#111111") : setStar("#ffffff");
+  };
+
+  console.log(onClick);
+  console.log(star);
   return (
     <div>
       <div>상호</div>
@@ -15,15 +28,28 @@ const Review = () => {
       </section>
       <div>별점</div>
       <section>
-        <div>star</div>
-        <div>star</div>
-        <div>star</div>
-        <div>star</div>
-        <div>star</div>
+        <button>
+          <Star onClick={changeStar} fill={star} />
+        </button>
+        <button>
+          <Star fill={star} />
+        </button>
+        <button>
+          <Star fill={star} />
+        </button>
+        <button>
+          <Star fill={star} />
+        </button>
+        <button>
+          <Star fill={star} />
+        </button>
       </section>
       <div>후기</div>
       <input></input>
-      <Button buttonStyle="main"> 등록하기 </Button>
+      <Button buttonStyle="main">
+        {" "}
+        <Link to={"/reviewDetail"} /> 등록하기{" "}
+      </Button>
     </div>
   );
 };
